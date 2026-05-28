f703511c32137c8894b91740d1ad16efe36a09aa
ayuhcl <ayushi-agarwal@hcltech.com>
2026-05-28 09:44:22 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,7 +1,7 @@
 module "eks" {
   source = "terraform-aws-modules/eks/aws"
 
-  cluster_name    = "my-eks-cluster"
+  cluster_name    = var.cluster_name
   cluster_version = "1.27"
 
   subnet_ids = module.vpc.public_subnets
@@ -9,10 +9,9 @@ module "eks" {
 
   eks_managed_node_groups = {
     default = {
-      desired_size = 1
-      max_size     = 2
-      min_size     = 1
-
+      desired_size   = 1
+      max_size       = 2
+      min_size       = 1
       instance_types = ["t3.micro"]
     }
   }
