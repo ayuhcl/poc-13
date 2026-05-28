14c0ff5310f3239a29d9c2ef7fd6d6c5776bf40c
ayuhcl <ayushi-agarwal@hcltech.com>
2026-05-28 12:49:11 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,20 +1,20 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 18.0"
+  version = "~> 19.0"
 
   cluster_name    = var.cluster_name
   cluster_version = "1.27"
 
-  vpc_id  = module.vpc.vpc_id
-  subnets = module.vpc.public_subnets
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.public_subnets
 
-  node_groups = {
+  eks_managed_node_groups = {
     default = {
-      desired_capacity = 1
-      max_capacity     = 2
-      min_capacity     = 1
+      desired_size   = 1
+      max_size       = 2
+      min_size       = 1
 
-      instance_types = ["t2.micro"]
+      instance_types = ["t3.micro"]
     }
   }
 }
