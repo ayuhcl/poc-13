a3821f92d7bb58eaadb08f13c3f8a3e6eb76f548
ayuhcl <ayushi-agarwal@hcltech.com>
2026-05-28 14:27:30 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -3,7 +3,9 @@ module "eks" {
   version = "~> 19.0"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.28"
+  cluster_version = "1.29"
+
+  create_kms_key = false 
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.public_subnets
