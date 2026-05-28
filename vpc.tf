5358c334bc3b1e8e043afadbfe2bd69f8c409171
ayuhcl <ayushi-agarwal@hcltech.com>
2026-05-28 15:35:54 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -2,11 +2,13 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.0"
 
-  name = "eks-vpc"
-  cidr = var.vpc_cidr
+  name = "${var.cluster_name}-vpc"
+  cidr = "10.0.0.0/16"
 
   azs            = ["us-east-1a", "us-east-1b"]
-  public_subnets = var.public_subnets
+  public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
 
   enable_nat_gateway = false
+
+  map_public_ip_on_launch = true  
 }
